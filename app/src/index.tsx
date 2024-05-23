01bad94f0f98e311e64fdb717317a9a59633a188
thaituongan <21130268@st.hcmuaf.edu.vn>
2024-05-23 22:04:50 +0700

    config-redux-store

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from "react-redux";
+import {store} from "./store/store";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -10,7 +12,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <App />
+<Provider store={store}>S
 
+</Provider>
   </React.StrictMode>
 );
 
