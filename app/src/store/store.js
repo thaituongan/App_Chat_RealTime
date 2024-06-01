ecf86a743750078475694c176da8f1a12ed7f007
thaituongan <21130268@st.hcmuaf.edu.vn>
2024-06-01 20:24:55 +0700

    add store redux

--- app/src/store/store.js
+++ app/src/store/store.js
@@ -1,4 +1,33 @@
-import {root} from "./RootReducer";
-import {configureStore} from "@reduxjs/toolkit";
+// src/store.js
+import { configureStore, createSlice } from '@reduxjs/toolkit';
 
-export const store = configureStore({reducer: root})
+const initialState = {
+    user: null,
+    messages: []
+};
+
+const chatSlice = createSlice({
+    name: 'chat',
+    initialState,
+    reducers: {
+        setUser: (state, action) => {
+            state.user = action.payload;
+        },
+        addMessage: (state, action) => {
+            state.messages.push(action.payload);
+        },
+        setMessages: (state, action) => {
+            state.messages = action.payload;
+        },
+    },
+});
+
+export const { setUser, addMessage, setMessages } = chatSlice.actions;
+
+const store = configureStore({
+    reducer: {
+        chat: chatSlice.reducer,
+    },
+});
+
+export default store;
