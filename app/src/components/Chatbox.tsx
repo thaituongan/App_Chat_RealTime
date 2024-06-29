5abebbe9abfa56d8360b7024a61ddba60d2c1be4
moclan01 <milepro98@gmail.com>
2024-06-29 13:15:20 +0700

    desgin message chatbox

--- app/src/components/Chatbox.tsx
+++ app/src/components/Chatbox.tsx
@@ -1,5 +1,7 @@
 import React, { FC } from "react";
 import "../styles/style.css";
+import { useSelector } from "react-redux";
+import { RootState } from "../store/store";
 
 interface Message {
     id: number;
@@ -15,15 +17,20 @@ interface ChatboxProps {
 }
 
 export const Chatbox: FC<ChatboxProps> = ({ messages }) => {
+    const username = useSelector((state: RootState) => state.user.username);
     return (
         <div className="chatbox-container">
             <div className="chatbox">
                 {messages.map((message, index) => (
-                    <div key={index} className="message">
-                        <div className="message-header">
+                    // <div key={index} className="message">
+                    <div
+                    key={index}
+                    className={`message ${message.name === username ? "my-message" : "other-message"}`}
+                >
+                        {/* <div className="message-header">
                             <span className="message-author">{message.name}</span>
                             <span className="message-timestamp">{new Date(message.createAt).toLocaleString()}</span>
-                        </div>
+                        </div> */}
                         <div className="message-content">{message.mes}</div>
                     </div>
                 ))}
@@ -31,3 +38,4 @@ export const Chatbox: FC<ChatboxProps> = ({ messages }) => {
         </div>
     );
 };
+
