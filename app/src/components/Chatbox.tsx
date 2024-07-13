fd4a32645d9fbb272d9870f130ddea8a819839fb
moclan01 <milepro98@gmail.com>
2024-07-13 23:50:55 +0700

    update message in chatbox

--- app/src/components/Chatbox.tsx
+++ app/src/components/Chatbox.tsx
@@ -19,6 +19,8 @@ interface ChatboxProps {
 
 const Chatbox: FC<ChatboxProps> = ({ messages, username, selectedUser, userStatus }) => {
     const chatEndRef = useRef<HTMLDivElement>(null);
+    const lastDisplayedDateAndName = useRef<string | null>(null); 
+
 
     const scrollToBottom = () => {
         if (chatEndRef.current) {
@@ -30,14 +32,25 @@ const Chatbox: FC<ChatboxProps> = ({ messages, username, selectedUser, userStatu
         scrollToBottom();
     }, [messages]);
 
+
     const renderMessage = (message: Message) => {
         const convertedMessage = decodeURIComponent(message.mes);
+        
+        const currentDateAndName = new Date(message.createAt).toDateString() + message.name;
+        const shouldDisplayDateAndName = currentDateAndName !== lastDisplayedDateAndName.current;
+        lastDisplayedDateAndName.current = currentDateAndName; 
+        const messageDate = new Date(message.createAt);
+        const messageTime = `${messageDate.getHours()}:${messageDate.getMinutes()}:${messageDate.getSeconds()}`;
+
         return (
             <div key={message.id} className={`message-box ${message.name === username ? "my-message" : "other-message"}`}>
-                <div className={`message-info ${message.name === username ? "my-message" : "other-message"}`}>
-                    <span className="message-sender">{message.name}</span>
-                    <span className="message-time">{new Date(message.createAt).toLocaleString()}</span>
-                </div>
+                {shouldDisplayDateAndName && (
+                    <div className={`message-info ${message.name === username ? "my-message" : "other-message"}`}>
+                        <span className="message-sender">{message.name}</span>
+                        {/* <span className="message-time">{new Date(message.createAt).toLocaleString()}</span> */}
+                        <span className="message-time" title={messageDate.toLocaleTimeString()}>{messageDate.toLocaleDateString()}</span>
+                    </div>
+                )}
                 <div className={`message ${message.name === username ? "my-message" : "other-message"}`}>
                     {convertedMessage}
                 </div>
